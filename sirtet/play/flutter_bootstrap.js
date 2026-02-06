31cb2604665343a8a2d4edb7bbe59131d6e196cf
Randall Li <randall.li@gmail.com>
2026-02-06 12:32:40 -0500

    Deploy Sirtet web build 2026-02-06 12:32:40

--- sirtet/play/flutter_bootstrap.js
+++ sirtet/play/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3840349314"
+    serviceWorkerVersion: "4059876253"
   }
 });
